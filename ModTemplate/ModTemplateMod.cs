19670ad65a5a49e983090278938be93d9b19e9bf
Autumn Turner <75217888+AutumnMooncat@users.noreply.github.com>
2026-07-20 20:55:29 -0300

    Add ModTemplate for easier project making

---
+++ ModTemplate/ModTemplateMod.cs
@@ -0,0 +1,29 @@
+﻿using FellSealAssetLoader;
+using FellSealAssetLoader.Tools;
+using FellSealAssetLoader.Util;
+using HarmonyLib;
+using MelonLoader;
+using ModTemplate;
+
+[assembly: MelonInfo(typeof(ModTemplateMod), "ModTemplate Mod", "0.0.1", "AuthorName")]
+[assembly: MelonAdditionalDependencies("FellSealAssetLoader")]
+namespace ModTemplate
+{
+    public class ModTemplateMod : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            LoggerInstance.Msg("ModTemplate initializing");
+        }
+    }
+
+    [HarmonyPatch]
+    public class Patches
+    {
+        [AssetInit]
+        public static void Init()
+        {
+            
+        }
+    }
+}
