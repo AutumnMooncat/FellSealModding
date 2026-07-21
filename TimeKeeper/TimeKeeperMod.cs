48faafe08ed14d7901887c86c8189fecfbd82d52
Autumn Turner <75217888+AutumnMooncat@users.noreply.github.com>
2026-07-20 21:04:15 -0300

    Make Timekeeper mod to test template and hold ideas

---
+++ TimeKeeper/TimeKeeperMod.cs
@@ -0,0 +1,29 @@
+﻿using FellSealAssetLoader;
+using FellSealAssetLoader.Tools;
+using FellSealAssetLoader.Util;
+using HarmonyLib;
+using MelonLoader;
+using TimeKeeper;
+
+[assembly: MelonInfo(typeof(TimeKeeperMod), "TimeKeeper Mod", "0.0.1", "")]
+[assembly: MelonAdditionalDependencies("FellSealAssetLoader")]
+
+namespace TimeKeeper
+{
+    public class TimeKeeperMod : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            LoggerInstance.Msg("TimeKeeper initializing");
+        }
+    }
+
+    [HarmonyPatch]
+    public class Patches
+    {
+        [AssetInit]
+        public static void Init()
+        {
+        }
+    }
+}
