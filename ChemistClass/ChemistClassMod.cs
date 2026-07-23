c96cdf0ef4455d3c78ef42ba6b63f36903ee88c5
Autumn Turner <75217888+AutumnMooncat@users.noreply.github.com>
2026-07-23 11:21:17 -0300

    Add Chemist Class

---
+++ ChemistClass/ChemistClassMod.cs
@@ -0,0 +1,30 @@
+﻿using FellSealAssetLoader;
+using FellSealAssetLoader.Tools;
+using FellSealAssetLoader.Util;
+using HarmonyLib;
+using MelonLoader;
+using ChemistClass;
+
+[assembly: MelonInfo(typeof(ChemistClassMod), "Chemist Class", "0.0.1", "Autumn Mooncat")]
+[assembly: MelonAdditionalDependencies("FellSealAssetLoader")]
+
+namespace ChemistClass
+{
+    public class ChemistClassMod : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            LoggerInstance.Msg("Chemist Class Melon Initializing");
+        }
+    }
+
+    [HarmonyPatch]
+    public class Patches
+    {
+        [AssetInit]
+        public static void Init()
+        {
+            Melon<ChemistClassMod>.Logger.Msg("Chemist Class Asset Initializing");
+        }
+    }
+}
